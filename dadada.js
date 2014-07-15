cda46ab0d68972512200c06c0cc85ce99b2e5530
justinphelps <justin.phelps@gmail.com>
2014-07-15 20:49:30 +0200

    DRY up pinocchio calls

--- dadada.js
+++ dadada.js
@@ -16,35 +16,28 @@ api.token = "7a7d184fb7a3fd6a0b32254b903998a7";
 var troopId = 4;
 var scoutId = 1;
 
+var scoutPost = function (strip, red,green,blue) {
+	// console.log(red + green + blue);
+	//url = "/v1/"+troopId+"/"+scoutId+"dadada.wcolor(" + strip + "," + red + "," + green + "," + blue + ")"
+	api.rest({
+	  url:"/v1/"+troopId+"/"+scoutId+"/command",
+	  data:{
+	    command:"dadada.wcolor(" + strip + "," + red + "," + green + "," + blue + ")"
+	  }
+	})		
+}
 
 $(document).ready(function(){
+$('.slider').slider()
+	.on('slide'), function(e){
+		console.log("slid");
+	}
+
   $("#red").click(function(){
-	api.rest({
-	  url:"/v1/"+troopId+"/"+scoutId+"/command",
-	  data:{
-	    command:"dadada.wcolor(0,255,0,0)"
-	  }
-	},function(error,result){
-	  // this is where you find out of the command was successful if(!error)
-	  // and read the result. for commands that print results.
-	  if(error) return $("#out").text('ERROR: '+(error.message||error+''));
-	  // setting the led does not print a result.
-	  $("#out").html("success!");
-	})
+		scoutPost(0,255,0,0);
   });
   $("#blue").click(function(){
-	api.rest({
-	  url:"/v1/"+troopId+"/"+scoutId+"/command",
-	  data:{
-	    command:"dadada.wcolor(0,0,0,255)"
-	  }
-	},function(error,result){
-	  // this is where you find out of the command was successful if(!error)
-	  // and read the result. for commands that print results.
-	  if(error) return $("#out").text('ERROR: '+(error.message||error+''));
-	  // setting the led does not print a result.
-	  $("#out").html("success!");
-	})
+		scoutPost(1,0,0,255);
   });
   $("#green").click(function(){
 	api.rest({
@@ -65,4 +58,3 @@ $(document).ready(function(){
 $("#out").text("sending command to troop "+troopId+" and scout "+scoutId);
 // after you finsih with this fiddle you might want to check out
 // http://jsfiddle.net/soldair/NYPTf/2/
-
--- index.php
+++ index.php
@@ -14,6 +14,7 @@
 
     <!-- Bootstrap core CSS -->
     <link href="bootstrap/css/bootstrap.css" rel="stylesheet">
+    <link href="slider/css/slider.css" rel="stylesheet">
 
     <!-- Just for debugging purposes. Don't actually copy this line! -->
     <!--[if lt IE 9]><script src="../../docs-assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
@@ -24,7 +25,7 @@
       <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
     <![endif]-->
     <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
-
+    <script src="slider/js/bootstrap-slider.js"></script>
 	<script src="https://api.pinocc.io/pinoccio.js" type="text/javascript"></script>
 	<script type="text/javascript" src="dadada.js"></script>
 	<link href="dadada.css" media="screen" rel="stylesheet" type="text/css" />
@@ -40,7 +41,57 @@
       <button type="button" class="btn btn-primary btn-lg btn-block" id="red">Red</button>
       <button type="button" class="btn btn-primary btn-lg btn-block" id="blue">Blue</button>
       <button type="button" class="btn btn-primary btn-lg btn-block" id="green">Green</button>
+
     </div>
+   <div class="well">
+   <p>
+      <b>R</b> 
+   <div class="slider slider-horizontal" id="RC" style="width: 314px;">
+      <div class="slider-track">
+         <div class="slider-selection" style="left: 0%; width: 50.19607843137255%;"></div>
+         <div class="slider-handle" style="left: 50.19607843137255%;"></div>
+         <div class="slider-handle hide" style="left: 0%;"></div>
+      </div>
+      <div class="tooltip top hide" style="top: -40px; left: 140.6156862745098px;">
+         <div class="tooltip-arrow"></div>
+         <div class="tooltip-inner">128</div>
+      </div>
+      <input type="text" class="span2" value="" data-slider-min="0" data-slider-max="255" data-slider-step="1" data-slider-value="128" data-slider-id="RC" id="R" data-slider-tooltip="hide" data-slider-handle="square">
+   </div>
+   </p>
+   <p>
+      <b>G</b> 
+   <div class="slider slider-horizontal" id="GC" style="width: 314px;">
+      <div class="slider-track">
+         <div class="slider-selection" style="left: 0%; width: 50.19607843137255%;"></div>
+         <div class="slider-handle round" style="left: 50.19607843137255%;"></div>
+         <div class="slider-handle round hide" style="left: 0%;"></div>
+      </div>
+      <div class="tooltip top hide" style="top: -40px; left: 140.6156862745098px;">
+         <div class="tooltip-arrow"></div>
+         <div class="tooltip-inner">128</div>
+      </div>
+      <input type="text" class="span2" value="" data-slider-min="0" data-slider-max="255" data-slider-step="1" data-slider-value="128" data-slider-id="GC" id="G" data-slider-tooltip="hide" data-slider-handle="round">
+   </div>
+   </p>
+   <p>
+      <b>B</b> 
+   <div class="slider slider-horizontal" id="BC" style="width: 314px;">
+      <div class="slider-track">
+         <div class="slider-selection" style="left: 0%; width: 50.19607843137255%;"></div>
+         <div class="slider-handle triangle" style="left: 50.19607843137255%;"></div>
+         <div class="slider-handle triangle hide" style="left: 0%;"></div>
+      </div>
+      <div class="tooltip top hide" style="top: -40px; left: 140.6156862745098px;">
+         <div class="tooltip-arrow"></div>
+         <div class="tooltip-inner">128</div>
+      </div>
+      <input type="text" class="span2" value="" data-slider-min="0" data-slider-max="255" data-slider-step="1" data-slider-value="128" data-slider-id="BC" id="B" data-slider-tooltip="hide" data-slider-handle="triangle">
+   </div>
+   </p>
+   <div id="RGB"></div>
+</div>
+
 	<div id="out">
 
 	</div>
