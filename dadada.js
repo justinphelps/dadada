9e5a9a380bc6089cdaae39ad8366e85685c5b4cb
Paul Morf <polymorf@hogmail.com>
2014-08-07 20:11:00 +0200

    rewrite 1

--- dadada.js
+++ dadada.js
@@ -1,24 +1,9 @@
 var api = pinoccioAPI();
-
-
-//replace this with your main access token to test running commands
 api.token = "7a7d184fb7a3fd6a0b32254b903998a7";
-
-
-// to make a "rest" or webservice call use api.rest
-// - url: the url of the rest service in the documentation. 
-// - method: for the http method GET/POST etc. 
-// - data: for service arguments
-// 
-// in this example we are making the led red!
-// 
-// set the troop an scout id to the correct ids for the troop you have connected.
 var troopId = 4;
 var scoutId = 1;
 
-var scoutPost = function (strip, red,green,blue) {
-	// console.log(red + green + blue);
-	//url = "/v1/"+troopId+"/"+scoutId+"dadada.wcolor(" + strip + "," + red + "," + green + "," + blue + ")"
+var scoutPost = function (strip, red, green, blue) {
 	api.rest({
 	  url:"/v1/"+troopId+"/"+scoutId+"/command",
 	  data:{
@@ -27,34 +12,28 @@ var scoutPost = function (strip, red,green,blue) {
 	})		
 }
 
-$(document).ready(function(){
-$('.slider').slider()
-	.on('slide'), function(e){
-		console.log("slid");
-	}
+$().ready(function(){
+	var RGBChange = function() {
+		$('#RGB').css('background', 'rgb('+r.getValue()+','+g.getValue()+','+b.getValue()+')');
+		var selectedChannel = $("#channel input[type='radio']:checked");
+		scoutPost(selectedChannel, r.getValue(), g.getValue(), b.getValue());
+	};
 
-  $("#red").click(function(){
-		scoutPost(0,255,0,0);
-  });
-  $("#blue").click(function(){
-		scoutPost(1,0,0,255);
-  });
-  $("#green").click(function(){
-	api.rest({
-	  url:"/v1/"+troopId+"/"+scoutId+"/command",
-	  data:{
-	    command:"dadada.wcolor(0,0,255,0)"
-	  }
-	},function(error,result){
-	  // this is where you find out of the command was successful if(!error)
-	  // and read the result. for commands that print results.
-	  if(error) return $("#out").text('ERROR: '+(error.message||error+''));
-	  // setting the led does not print a result.
-	  $("#out").html("success!");
-	})
-  });
+
+	var r = $('#R').slider()
+			.on('slide', RGBChange)
+			.data('slider');
+	var g = $('#G').slider()
+			.on('slide', RGBChange)
+			.data('slider');
+	var b = $('#B').slider()
+			.on('slide', RGBChange)
+			.data('slider');
+	var channel = $()
+
+	$('#ex1').slider({
+		formater: function(value) {
+			return 'Current value: ' + value;
+		}
+	});
 });
-
-$("#out").text("sending command to troop "+troopId+" and scout "+scoutId);
-// after you finsih with this fiddle you might want to check out
-// http://jsfiddle.net/soldair/NYPTf/2/
